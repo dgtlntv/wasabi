94694f50d8f92f2aafb3d3bb6c6b67f5321deb12
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-05-21 11:58:33 +0200

    add for file path issue

---
+++ src/Hooks/useOutsideClick.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+export default function useOutsideClick(ref, callback) {
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            callback()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside)
+        return () => {
+            document.removeEventListener("click", handleClickOutside)
+        }
+    }, [ref, callback])
+}
