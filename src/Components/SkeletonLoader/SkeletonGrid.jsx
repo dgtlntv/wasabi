d28f0698667ccb6ee833cb5d34efc7d876206d82
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-05-21 11:12:21 +0200

    Update SkeletonGrid.jsx

--- src/Components/SkeletonLoader/SkeletonGrid.jsx
+++ src/Components/SkeletonLoader/SkeletonGrid.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 export default function SkeletonGrid({ rows, columns, squareColors }) {
+    const [showDetails, setShowDetails] = useState(false)
+
     const getDelay = (row, col) => {
         const delayFactor = 0.11
         return `${(row + col) * delayFactor}s`
@@ -19,15 +23,63 @@ export default function SkeletonGrid({ rows, columns, squareColors }) {
                         style={{
                             opacity: 0,
                             animation: `appear 0.5s ease forwards ${getDelay(row, col)}`,
-                        }}>
+                        }}
+                    >
                         <div
                             style={{
                                 backgroundColor: color,
                                 aspectRatio: "1/1",
                                 padding: "10px",
                                 transition: "background-color 2s ease",
+                                color: `${
+                                    squareColors &&
+                                    squareColors[row] &&
+                                    squareColors[row][col] &&
+                                    squareColors[row][col]["isDark"]
+                                        ? "white"
+                                        : "black"
+                                }`,
+                                border: `${
+                                    squareColors &&
+                                    squareColors[row] &&
+                                    squareColors[row][col] &&
+                                    squareColors[row][col]["isPrimary"]
+                                        ? "4px solid #FF0000"
+                                        : ""
+                                }`,
                             }}
-                        />
+                        >
+                            {squareColors && squareColors[row] && squareColors[row][col] && showDetails ? (
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        height: "100%",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    <div>
+                                        <p style={{ margin: "0", padding: "0" }}>
+                                            L: {Math.round(squareColors[row][col]["color"]["l"] * 100) / 100}
+                                        </p>
+                                        <p style={{ margin: "0", padding: "0" }}>
+                                            C: {Math.round(squareColors[row][col]["color"]["c"] * 100) / 100}
+                                        </p>
+                                        <p style={{ margin: "0", padding: "0" }}>
+                                            H: {Math.round(squareColors[row][col]["color"]["h"] * 100) / 100}
+                                        </p>
+                                        <p style={{ margin: "0", padding: "0" }}>
+                                            Contrast: {Math.round(squareColors[row][col]["contrastValue"] * 100) / 100}
+                                        </p>
+                                    </div>
+                                    <p style={{ margin: "0", padding: "0" }}>
+                                        Gamut: {squareColors[row][col]["colorGamut"]}
+                                    </p>
+                                </div>
+                            ) : null}
+                        </div>
                     </div>
                 )
             }
@@ -42,7 +94,10 @@ export default function SkeletonGrid({ rows, columns, squareColors }) {
                 gridTemplateColumns: `repeat(${columns}, minmax(auto, 1fr))`,
                 gap: "10px",
                 padding: "20px",
-            }}>
+                cursor: "pointer",
+            }}
+            onClick={() => setShowDetails((d) => !d)}
+        >
             {renderGridItems()}
         </div>
     )
